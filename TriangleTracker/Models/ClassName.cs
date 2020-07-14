6123f4b89aea901058966ec56e72641ebff89dba
Taylor Phillips <taylorphillips133@gmail.com>
2020-07-14 11:24:37 -0700

    added Exe output in the csproj file, added classes
    Co-Authored-By: Teresa Rosinski <trosinski89@gmail.com>
    Co-Authored-By: Megan Hepner <megan.hepner@gmail.com>

--- TriangleTracker/Models/ClassName.cs
+++ TriangleTracker/Models/ClassName.cs
@@ -1,7 +1,37 @@
-namespace TriangleTracker
+using System; 
+using System.Collections.Generic;
+
+namespace Triangle
 {
-  public class ClassName
+  public class TriangleKind
   {
-    
+   
+  }
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Enter 3 numbers: ");
+      string userInput = Console.ReadLine();
+      int lengths = int.Parse(userInput);
+    }
   }
 }
+
+    // if (this.side1 === this.side2 && this.side1 === this.side3)
+      // {
+      //   return "Equilateral Triangle";
+    //   }
+    //   else if (this.side1 === this.side2 || this.side1 === this.side3)
+    //   {
+    //     return "Isosceles Triangle";
+    //   }
+    //   else if (this.side1 != this.side2 && this.side1 != this.side3 && this.side2 != this.side3)
+    //   {
+    //     return "Scalene Triangle";
+    //   }
+    //   else
+    //   {
+    //     return "Not triangle";
+    //   }
+    // 
